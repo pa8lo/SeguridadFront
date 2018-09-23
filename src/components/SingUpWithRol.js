9427334af9a6a23b1a26ee65b5d72d47d192b870
pablo.rozek@davinci.edu.ar <pablo.rozek@davinci.edu.ar>
2018-09-23 17:12:38 -0300

    Subido registro de usuarios con rol y lista de usuarios

--- src/components/SingUpWithRol.js
+++ src/components/SingUpWithRol.js
@@ -1,96 +1,238 @@
-/*mport React from 'react';
-import {Button,FormGroup,FormControl,HelpBlock,ControlLabel} from 'react-bootstrap';
+import React from 'react';
 import axios from 'axios';
-//this.props.history.push('/SignUpOk');   
+import {DropdownButton,MenuItem,Button,FormGroup,FormControl,HelpBlock,ControlLabel} from 'react-bootstrap';
 
-class SingUpWithRol extends React.Component {
-    constructor(props, context) {
-      super(props, context);
-  
+export default class SingUpWithRol extends React.Component {
+  constructor(props, context) {
+    super(props, context);
       this.handleDni = this.handleDni.bind(this);
-      this.handlePassword = this.handlePassword.bind(this);
+      this.handleEmail = this.handleEmail.bind(this);
+      this.handleRol = this.handleRol.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleLastName = this.handleLastName.bind(this);
+      this.handleName =this.handleName.bind(this);
+      this.handleDepartment=this.handleDepartment.bind(this);
+      this.handleAdress=this.handleAdress.bind(this);
+      this.handleFloor=this.handleFloor.bind(this);
+      this.handleNumber=this.handleNumber.bind(this);
       this.state = {
         Dni: '',
-        Password:''
+        Email:'',
+        rols:[],
+        rol:'',
+        LastName:'',
+        Name:'',
+        Department:'',
+        Adress:'',
+        Floor:'',
+        Number:''
       };
+  }
+
+  async componentDidMount() {
+   var accessToken =  localStorage.getItem('access-token');
+   console.log(accessToken)
+   await  axios.get('http://localhost:1337/Rol/Rols',
+    {headers: {'access-token': accessToken}})
+      .then(res => {
+        const rols = res.data.roles;
+        console.log( res.data)
+        this.setState({  rols });
+        
+      })
+  }
+  async handleSubmit(e){
+    console.log(this.state)
+    try {
+      
+                   if(this.state.Dni !== '' && this.state.rol !== '' &&
+                    this.state.Email !== '' && this.state.LastName !== ''&&
+                     this.state.Name !== ''){
+                       var data = {
+                        User:{
+                          Dni: this.state.Dni,
+                          Email: this.state.Email,
+                          rol: this.state.rol,
+                          LastName: this.state.LastName,
+                          Name: this.state.Name,
+                         },
+                        Adress:{
+                          Department: this.state.Department,
+                          Adress: this.state.Adress,
+                          Floor: this.state.Floor,
+                        },
+                        Number:{
+                          Number: this.state.Number
+                        }
+                        
+                       }
+                   try {
+                     var accessToken =  localStorage.getItem('access-token');
+                      const res = await axios.post("http://localhost:1337/User/SingUp",data,{headers: {'access-token': accessToken}});                     
+                      } catch (error) {
+                       alert(error)
+                   } 
+                   
+                   }else{
+                       return 'warning'
+                       
+                   }
+               } catch (error) {
+                   console.log(error)
+               }
+          }
+  
+
+  handleRol(e){
+    this.setState({ rol: e.target.value });
+  }
+
+  handleDni(e) {
+    this.setState({ Dni: e.target.value });
+
+  }
+    handleLastName(e){
+      this.setState({ LastName: e.target.value });
     }
-    componentDidMount() {
-        axios.get(`http://localhost:1337/User/users`)
-          .then(res => {
-            const persons = res.data;
-            alert(JSON.stringify(res.data))
-            this.setState({ persons });
-          })
+
+    handleName(e){
+      this.setState({ Name: e.target.value });
+    }
+
+    handleAdress(e){
+      this.setState({ Adress: e.target.value });
+    }
+
+    handleDepartment(e){
+      this.setState({ Department: e.target.value });
+    }
+
+    handleFloor(e){
+      this.setState({ Floor: e.target.value });
+    }
+
+    handleNumber(e){
+      this.setState({ Number: e.target.value });
+    }
+
+    handleEmail(e) {
+        this.setState({ Email: e.target.value });
       }
+
     
-    getValidationState() {
-      const length = this.state.Dni.length;
-      if (length == 0) return 'warning';
-       return null;
-    }
-  
-    handleDni(e) {
-      this.setState({ Dni: e.target.value });
+   rolsList() {
+    console.log(this.state.rols)
+    const rols = this.state.rols
+    console.log(rols)
+    const listRols = rols.map((rol) =>
+    <option value={rol.id}>{rol.Name}</option>
+    
+    );
+    return (
+    <FormGroup controlId="formControlsSelect">
+    <ControlLabel>Select</ControlLabel>
+      <FormControl
+      controlId="formControlsSelect"
+      componentClass="select" placeholder="select"
+      value={this.state.rol}
+      placeholder="Enter text"
+      onChange={this.handleRol}>
+        <option value="">Seleccionar Rol</option>
+        {listRols} 
+      </FormControl>
+      </FormGroup>
+    );
+
+  }
  
-    }
-
-    handlePassword(e) {
-        this.setState({ Password: e.target.value });
-      }
-    async handleSubmit(e){
-         try {
-
-             if(this.state.Dni !== '' && this.state.Password !== ''){
-                 var data = {
-                     Dni:this.state.Dni,
-                     Password:this.state.Password
-                 }
-             try {
-                const res = await axios.post("http://localhost:1337/User/login",data);
-                localStorage.setItem('access-token', res.data.token);
-                alert( res.data.token)
-                } catch (error) {
-                 alert(error)
-             } 
-             
-             }else{
-                alert("error")
-                 return 'warning'
-                 
-             }
-         } catch (error) {
-             alert(error)
-         }
-    }
-  
-
   render() {
-    return (<form>
-        <FormGroup
-          controlId="formDni"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel>Dni</ControlLabel>
-          <FormControl
-            type="string"
-            value={this.state.Dni}
-            placeholder="Enter text"
-            onChange={this.handleDni}
-          />
+    return (
+      <ul>    
+          <FormGroup>
+            <ControlLabel>Dni</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Dni}
+              placeholder="Enter text"
+              onChange={this.handleDni}
+            />
+           </FormGroup>
+           <FormGroup>
+            <ControlLabel>Nombre</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Name}
+              placeholder="Enter text"
+              onChange={this.handleName}
+            />
+           </FormGroup>
+           <FormGroup>
+            <ControlLabel>Apellido</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.LastName}
+              placeholder="Enter text"
+              onChange={this.handleLastName}
+            />
+           </FormGroup>
           <FormControl.Feedback />
-          <ControlLabel>Password</ControlLabel>
+          <ControlLabel>Email</ControlLabel>
+          <FormGroup>
           <FormControl
-            type="password"
-            value={this.state.Password}
+            type="email"
+            value={this.state.Email}
             placeholder="Enter text"
-            onChange={this.handlePassword}
+            onChange={this.handleEmail}
           />
+          </FormGroup>   
+          {this.rolsList()}
+          <FormControl.Feedback />
+          <FormGroup>
+            <ControlLabel>Direcciòn</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Adress}
+              placeholder="Enter text"
+              onChange={this.handleAdress}
+            />
+           </FormGroup>
+          <FormControl.Feedback />
+          <FormGroup>
+            <ControlLabel>Departamento</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Department}
+              placeholder="Enter text"
+              onChange={this.handleDepartment}
+            />
+           </FormGroup>
+          <FormControl.Feedback />
+          <FormGroup>
+            <ControlLabel>Piso</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Floor}
+              placeholder="Enter text"
+              onChange={this.handleFloor}
+            />
+           </FormGroup>
+          <FormControl.Feedback />
+          <FormGroup>
+            <ControlLabel>Telefono</ControlLabel>        
+            <FormControl
+              type="string"
+              value={this.state.Number}
+              placeholder="Enter text"
+              onChange={this.handleNumber}
+            />
+           </FormGroup>
           <FormControl.Feedback />
           <Button type="submit" onClick={this.handleSubmit}>Iniciar Sesion</Button>
-        </FormGroup>
-      </form>
+          
+        
+              
+             
+      </ul>
     )
   }
 }
-export default LoginForm;*/
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,241 +1,73 @@
 import React from 'react';
 import axios from 'axios';
 import {DropdownButton,MenuItem,Button,FormGroup,FormControl,HelpBlock,ControlLabel} from 'react-bootstrap';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default class Users extends React.Component {
   constructor(props, context) {
     super(props, context);
-      this.handleDni = this.handleDni.bind(this);
-      this.handleEmail = this.handleEmail.bind(this);
-      this.handleRol = this.handleRol.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleLastName = this.handleLastName.bind(this);
-      this.handleName =this.handleName.bind(this);
-      this.handleDepartment=this.handleDepartment.bind(this);
-      this.handleAdress=this.handleAdress.bind(this);
-      this.handleFloor=this.handleFloor.bind(this);
-      this.handleNumber=this.handleNumber.bind(this);
+   //   this.handleUsers = this.handleDni.bind(this);
       this.state = {
-        Dni: '',
-        Email:'',
-        rols:[],
-        rol:'',
-        LastName:'',
-        Name:'',
-        Department:'',
-        Adress:'',
-        Floor:'',
-        Number:''
+        users:[]
       };
   }
 
   async componentDidMount() {
    var accessToken =  localStorage.getItem('access-token');
    console.log(accessToken)
-   await  axios.get('http://localhost:1337/Rol/Rols',
-    {headers: {'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiQWRtaW4iLCJJZCI6MSwiQXV0aG9yaXphdGlvbnMiOlt7ImNyZWF0ZWRBdCI6MTUzNjk1NTExMjA4OCwidXBkYXRlZEF0IjoxNTM2OTU1MTEyMDg4LCJpZCI6MSwiTmFtZSI6IlVzdWFyaW8iLCJEZXNjcmlwdGlvbiI6IlBlcm1pdGUgbGEgY3JlYWNpb24gZGUgdmVyICwgY3JlYXIsZWRpdGFyIHkgZWxpbWluYXIgdXN1YXJpbyJ9XSwiaWF0IjoxNTM3Mjc5MzkyfQ.iWHt4la5LfLJojip6B9bTH7N3O3UczEYe3MgAiTRcF4'}})
+   await  axios.get('http://localhost:1337/User/users',
+    {headers: {'access-token': accessToken}})
       .then(res => {
-        const rols = res.data.roles;
+        const users = res.data;
         console.log( res.data)
-        this.setState({  rols });
+        this.setState({  users });
         
       })
   }
-  async handleSubmit(e){
-    console.log(this.state)
-    try {
-      
-                   if(this.state.Dni !== '' && this.state.rol !== '' &&
-                    this.state.Email !== '' && this.state.LastName !== ''&&
-                     this.state.Name !== ''){
-                       var data = {
-                        User:{
-                          Dni: this.state.Dni,
-                          Email: this.state.Email,
-                          rol: this.state.rol,
-                          LastName: this.state.LastName,
-                          Name: this.state.Name,
-                         },
-                        Adress:{
-                          Department: this.state.Department,
-                          Adress: this.state.Adress,
-                          Floor: this.state.Floor,
-                        },
-                        Number:{
-                          Number: this.state.Number
-                        }
-                        
-                       }
-                   try {
-                      var token = localStorage.getItem('access-token');
-                      alert(JSON.stringify (data))
-                      const res = await axios.post("http://localhost:1337/User/SingUp",data,{headers: {'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiQWRtaW4iLCJJZCI6MSwiQXV0aG9yaXphdGlvbnMiOlt7ImNyZWF0ZWRBdCI6MTUzNjk1NTExMjA4OCwidXBkYXRlZEF0IjoxNTM2OTU1MTEyMDg4LCJpZCI6MSwiTmFtZSI6IlVzdWFyaW8iLCJEZXNjcmlwdGlvbiI6IlBlcm1pdGUgbGEgY3JlYWNpb24gZGUgdmVyICwgY3JlYXIsZWRpdGFyIHkgZWxpbWluYXIgdXN1YXJpbyJ9XSwiaWF0IjoxNTM3Mjc5MzkyfQ.iWHt4la5LfLJojip6B9bTH7N3O3UczEYe3MgAiTRcF4'}});                     
-                      alert(JSON.stringify( res.data))
-                      } catch (error) {
-                       alert(error)
-                   } 
-                   
-                   }else{
-                      alert("error")
-                       return 'warning'
-                       
-                   }
-               } catch (error) {
-                   console.log(error)
-               }
-          }
-  
-
-  handleRol(e){
-    this.setState({ rol: e.target.value });
-  }
-
-  handleDni(e) {
-    this.setState({ Dni: e.target.value });
-
-  }
-    handleLastName(e){
-      this.setState({ LastName: e.target.value });
-    }
-
-    handleName(e){
-      this.setState({ Name: e.target.value });
-    }
-
-    handleAdress(e){
-      this.setState({ Adress: e.target.value });
-    }
-
-    handleDepartment(e){
-      this.setState({ Department: e.target.value });
-    }
-
-    handleFloor(e){
-      this.setState({ Floor: e.target.value });
-    }
-
-    handleNumber(e){
-      this.setState({ Number: e.target.value });
-    }
-
-    handleEmail(e) {
-        this.setState({ Email: e.target.value });
-      }
-
-    
-   rolsList() {
-    console.log(this.state.rols)
-    const rols = this.state.rols
-    console.log(rols)
-    const listRols = rols.map((rol) =>
-    <option value={rol.id}>{rol.Name}</option>
-    
-    );
+  async userDetail(id){
+      alert(id)
+  } 
+   usersFills() {
+    const listUser = this.state.users.map((user) =>
+      <TableRow key={user.id} onClick={() => this.userDetail(user.id)} >
+        <TableCell component="th" scope="row"> 
+          {user.Dni}
+        </TableCell>
+        <TableCell >{user.Name}</TableCell>
+        <TableCell >{user.LastName}</TableCell>
+        <TableCell >{user.Email}</TableCell>
+      </TableRow>
+    )
     return (
-    <FormGroup controlId="formControlsSelect">
-    <ControlLabel>Select</ControlLabel>
-      <FormControl
-      controlId="formControlsSelect"
-      componentClass="select" placeholder="select"
-      value={this.state.rol}
-      placeholder="Enter text"
-      onChange={this.handleRol}>
-        <option value="">Seleccionar Rol</option>
-        {listRols} 
-      </FormControl>
-      </FormGroup>
-    );
+    <Paper >
+    <Table >
+      <TableHead>
+        <TableRow>
+          <TableCell>Documento de identidad</TableCell>
+          <TableCell >Nombre</TableCell>
+          <TableCell >Apellido</TableCell>
+          <TableCell >Email</TableCell>
+          <TableCell >Acciones</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {listUser}
 
+      </TableBody>
+    </Table>
+  </Paper>  
+    );
   }
- 
   render() {
     return (
-      <ul>    
-          <FormGroup>
-            <ControlLabel>Dni</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Dni}
-              placeholder="Enter text"
-              onChange={this.handleDni}
-            />
-           </FormGroup>
-           <FormGroup>
-            <ControlLabel>Nombre</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Name}
-              placeholder="Enter text"
-              onChange={this.handleName}
-            />
-           </FormGroup>
-           <FormGroup>
-            <ControlLabel>Apellido</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.LastName}
-              placeholder="Enter text"
-              onChange={this.handleLastName}
-            />
-           </FormGroup>
-          <FormControl.Feedback />
-          <ControlLabel>Email</ControlLabel>
-          <FormGroup>
-          <FormControl
-            type="email"
-            value={this.state.Email}
-            placeholder="Enter text"
-            onChange={this.handleEmail}
-          />
-          </FormGroup>   
-          {this.rolsList()}
-          <FormControl.Feedback />
-          <FormGroup>
-            <ControlLabel>Direcciòn</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Adress}
-              placeholder="Enter text"
-              onChange={this.handleAdress}
-            />
-           </FormGroup>
-          <FormControl.Feedback />
-          <FormGroup>
-            <ControlLabel>Departamento</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Department}
-              placeholder="Enter text"
-              onChange={this.handleDepartment}
-            />
-           </FormGroup>
-          <FormControl.Feedback />
-          <FormGroup>
-            <ControlLabel>Piso</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Floor}
-              placeholder="Enter text"
-              onChange={this.handleFloor}
-            />
-           </FormGroup>
-          <FormControl.Feedback />
-          <FormGroup>
-            <ControlLabel>Telefono</ControlLabel>        
-            <FormControl
-              type="string"
-              value={this.state.Number}
-              placeholder="Enter text"
-              onChange={this.handleNumber}
-            />
-           </FormGroup>
-          <FormControl.Feedback />
-          <Button type="submit" onClick={this.handleSubmit}>Iniciar Sesion</Button>
-          
-        
-              
-             
-      </ul>
+      this.usersFills()
     )
   }
+
 }
